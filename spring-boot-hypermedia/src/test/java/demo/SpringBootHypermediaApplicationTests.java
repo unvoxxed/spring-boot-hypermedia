9f06038926e3f700bc0e0beb23a857687b38f515
Dave Syer <dsyer@pivotal.io>
2015-06-20 10:26:03 +0100

    Add JSON path tests

--- spring-boot-hypermedia/src/test/java/demo/SpringBootHypermediaApplicationTests.java
+++ spring-boot-hypermedia/src/test/java/demo/SpringBootHypermediaApplicationTests.java
@@ -1,18 +1,68 @@
 package demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.mvc.MvcEndpoint;
+import org.springframework.boot.actuate.endpoint.mvc.MvcEndpoints;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import demo.SpringBootHypermediaApplicationTests.SpringBootHypermediaApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootHypermediaApplication.class)
 @WebAppConfiguration
 public class SpringBootHypermediaApplicationTests {
 
+	@Autowired
+	private WebApplicationContext context;
+
+	@Autowired
+	private MvcEndpoints mvcEndpoints;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void links() throws Exception {
+		this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk()).andExpect(jsonPath("$._links").exists());
+	}
+
+	@Test
+	public void endpoints() throws Exception {
+		for (MvcEndpoint endpoint : this.mvcEndpoints.getEndpoints()) {
+			String path = endpoint.getPath();
+			path = path.startsWith("/") ? path.substring(1) : path;
+			this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$._links.%s.href", path).exists());
+		}
+	}
+
+	@SpringBootApplication
+	public static class SpringBootHypermediaApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(SpringBootHypermediaApplication.class, args);
+		}
 	}
 
 }
--- spring-boot-sample-hypermedia-jpa/src/main/java/demo/JpaHypermediaApplication.java
+++ spring-boot-sample-hypermedia-jpa/src/main/java/demo/JpaHypermediaApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHypermediaApplication {
+public class JpaHypermediaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHypermediaApplication.class, args);
+		SpringApplication.run(JpaHypermediaApplication.class, args);
 	}
+
 }
