4f13f94820e0e52f7855ae6e470c775e862cd91b
Dave Syer <dsyer@pivotal.io>
2015-06-22 15:16:01 +0100

    Add template for default endpoints docs

--- spring-boot-hypermedia-docs/src/test/java/demo/SpringBootHypermediaApplication.java
+++ spring-boot-hypermedia-docs/src/test/java/demo/SpringBootHypermediaApplication.java
@@ -1,10 +1,19 @@
 package demo;
 
+import groovy.text.GStringTemplateEngine;
+import groovy.text.TemplateEngine;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootHypermediaApplication {
+
+	@Bean
+	public TemplateEngine groovyTemplateEngine() {
+		return new GStringTemplateEngine();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHypermediaApplication.class, args);
